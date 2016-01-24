6b73a223335deb12a319134fa7e59158df07f19b
Anne G <annasazi@gmail.com>
2016-01-24 03:06:51 +0000

    ExtractedResource commodities can only be minerals or metals

--- app/models/extracted_resource.rb
+++ app/models/extracted_resource.rb
@@ -3,4 +3,15 @@ class ExtractedResource < ActiveRecord::Base
   belongs_to :mining_entry
 
   validates :commodity, :mining_entry, presence: true
+  validate :is_mineable
+
+  delegate :category, to: :commodity, prefix: true, allow_nil: true
+
+  private
+
+  def is_mineable
+    unless ["minerals", "metals"].include?(commodity_category)
+      errors.add(:commodity, "can only extract minerals and metals")
+    end
+  end
 end
