47c772b11a7c5a5453b0d354399705e0e9955f76
Anne G <annasazi@gmail.com>
2016-01-25 20:51:26 +0000

    Add mineable scope on Commodity

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -3,4 +3,6 @@ class Commodity < ActiveRecord::Base
   enum category: [:chemicals, :consumer_items, :foods, :industrial_materials, 
                   :legal_drugs, :machinery, :medicines, :metals, :minerals, 
                   :salvage, :slavery, :technology, :textiles, :waste, :weapons]
+
+  scope :mineable, -> { where(category: [7,8]) } #TODO: there should be a better way to do this
 end
--- spec/models/commodity_spec.rb
+++ spec/models/commodity_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 describe Commodity do
   it { is_expected.to validate_presence_of(:name) }
+
+  describe "scopes" do
+    let!(:mineral) { create(:commodity, name: "Painite", category: "minerals")        }
+    let!(:metal)   { create(:commodity, name: "Platinum", category: "metals")         }
+    let!(:weapon)  { create(:commodity, name: "Reactive Armor", category: "weapons") }
+
+    describe ".mineable" do
+      it "gets minerals and metals" do
+        expect(Commodity.mineable).to contain_exactly(mineral, metal)
+      end
+
+      it "ignores the rest" do
+        expect(Commodity.mineable).to_not include(weapon)
+      end
+    end
+  end
 end
