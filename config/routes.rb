83e893b277aef36d3031ae469cf05cfc4b5d39d5
Anne G <annasazi@gmail.com>
2016-01-27 01:32:00 +0000

    Create a page to view the ships cargo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :cmdrs do
     resources :entries
+    resources :ships, only: [:show]
   end
 
   resources :extracted_resources, only: [:create, :destroy]
---
+++ spec/controllers/ships_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe ShipsController do
+  let!(:cmdr) { create(:cmdr) }
+
+  before do
+    @request.session['current_cmdr_id'] = cmdr.id
+  end
+
+  describe "GET #show" do
+    let!(:ship) { create(:ship, cmdr: cmdr) }
+    it "returns http success" do
+      get :show, id: ship.id, cmdr_id: cmdr.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns the ship" do
+      get :show, id: ship.id, cmdr_id: cmdr.id
+      expect(assigns(:ship)).to eq(ship)
+    end
+  end
+end
