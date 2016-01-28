6c8db8b0c169be57a4b5f9cd306713ae60c5dd8e
Anne G <annasazi@gmail.com>
2016-01-28 22:53:33 +0000

    Add cargo_items to Ships

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,5 @@
 class Ship < ActiveRecord::Base
   belongs_to :cmdr
+  has_many :cargo_items, class_name: 'CommodityExchange'
   validates :model, presence: true
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe Ship do
   it { is_expected.to belong_to :cmdr }
+  it { is_expected.to have_many :cargo_items }
   it { is_expected.to validate_presence_of(:model) }
 end
