ba7322fd7cdde64c4f4387517a70f4128d897f6a
Anne G <annasazi@gmail.com>
2016-03-04 23:30:15 +0000

    Fix broken specs

--- spec/factories/systems.rb
+++ spec/factories/systems.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :system do
-    name  'Guathiti'
-    x      93.90625
-    y     -94.15625
-    z     -15.09375
+    sequence(:name) {|x| "system #{x}" }
+    x                93.90625
+    y               -94.15625
+    z               -15.09375
   end
 
 end
