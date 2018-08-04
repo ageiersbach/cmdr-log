40e0c128cbcca830bdc47bb2543f2022ca352d3c
Anne Geiersbach <annasazi@gmail.com>
2018-08-04 16:20:32 -0400

    Add link to token#index to header

--- test/controllers/commanders_controller_test.rb
+++ test/controllers/commanders_controller_test.rb
@@ -3,12 +3,6 @@ require 'test_helper'
 class CommandersControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
-  test "index" do
-    sign_in commanders(:one)
-    get :index
-    assert_response :success
-  end
-
   test "show" do
     c = commanders(:one)
     sign_in commanders(:one)
